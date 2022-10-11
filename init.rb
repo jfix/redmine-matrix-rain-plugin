7b23ae7c5da96df78445a2b97147386421203c45
Jakob Fix <jakob.fix@gmail.com>
2022-10-11 14:17:15 +0200

    Bump version number

--- init.rb
+++ init.rb
@@ -4,5 +4,5 @@ Redmine::Plugin.register :matrix_rain_konami do
   name 'Matrix Rain plugin'
   author 'Jakob Fix'
   description 'Matrix Rain for those who know the right kombination'
-  version '1.0.0'
+  version '1.1.0'
 end
